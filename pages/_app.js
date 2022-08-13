84004c51ac29c665b9e4f65dca2ca46c00282e2f
Pisanuwat <pisanuwat.b@gmail.com>
2022-08-14 02:48:26 +0700

    add firebase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import { useRouter } from 'next/router';
+import { AuthProvider } from '../src/lib/auth-service';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import NavBar from '../components/Layout/NavBar';
+config.autoAddCss = false;
+
+function App({ Component, pageProps }) {
+  const router = useRouter();
+  const showNav = (router.pathname === '/practice' || router.pathname === '/signin') ? false : true;
+  return (
+    <>
+      <AuthProvider>
+        {showNav && <NavBar />}
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default App;
