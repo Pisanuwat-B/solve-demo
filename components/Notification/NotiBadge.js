fd2075c56eacb6357e6b32ce48e643d98485a557
pisanuwat <pisanuwat.b@gmail.com>
2022-10-14 10:15:13 +0700

    fix minor error

--- components/Notification/NotiBadge.js
+++ components/Notification/NotiBadge.js
@@ -1,17 +1,15 @@
-// import Link from 'next/link';
+import Link from 'next/link';
 import styles from './NotiBadge.module.css';
 
 const NotiBadge = (props) => {
   return (
-    // <Link href="/course">
-      <div className={styles['notibox']} >
-        <div>
-          <div>Student has question</div>
-          <div>Question ID: {props.questionId}</div>
-          <div>Student question: {props.question}</div>
-        </div>
+    <div className={styles['notibox']}>
+      <div>
+        <div>Student has question</div>
+        <div>Question ID: {props.questionId}</div>
+        <div>Student question: {props.question}</div>
       </div>
-    {/* </Link> */}
+    </div>
   );
 };
 
